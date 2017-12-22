410554078a2adcd3d8d4f6faf162e9632ccdaa01
Ömer Cinbat <ocinbat@outlook.com>
2017-12-22 14:26:13 +0300

    Modül oluşturuldu.

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports.integer = function (min, max) {
+    if (max === undefined) {
+        max = min;
+        min = 0;
+    }
+
+    if (typeof min !== 'number' || typeof max !== 'number') {
+        throw new TypeError('Expected all arguments to be numbers');
+    }
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
